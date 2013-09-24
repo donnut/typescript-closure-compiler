152d3446e69b259775da11781aafb1dd89b35906
Evan Wallace <evan@figma.com>
2013-09-23 19:54:05 -0700

    added typescript emitter code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# TypeScript Closure Compiler
 
+This patches the TypeScript 0.9.1-1 compiler to generate JSDoc annotations ready for Google Closure Compiler.
--- typescript-closure-compiler.js
+++ typescript-closure-compiler.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+
+var TSC = './node_modules/typescript/bin/tsc.js';
+
+eval(require('fs')
+  .readFileSync(TSC, 'utf8')
+  .replace(/batch\.batchCompile\(\);\s*$/, '')
+  .replace(/\btsc\b/g, 'tscc'));
+
+eval(require('fs')
+  .readFileSync('./emitter.js', 'utf8'));
+
+process.mainModule.filename = require('path').resolve(TSC);
+TypeScript.Indenter.indentStepString = '  ';
+TypeScript.Indenter.indentStep = 2;
+batch.batchCompile();
