15319abbfc5eef27285c1032688305b6c178acf0
Evan Wallace <evan@figma.com>
2013-09-26 11:25:03 -0700

    emit casts around calls on functions with multiple signatures

---
+++ test/autocast.ts
@@ -0,0 +1,5 @@
+var canvasA = document.createElement('canvas');
+var contextA = canvasA.getContext('2d');
+
+var canvasB = document.createElement('foo');
+var contextB = (<HTMLCanvasElement>canvasB).getContext('foo');
